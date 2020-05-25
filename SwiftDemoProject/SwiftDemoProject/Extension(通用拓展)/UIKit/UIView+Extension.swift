4b4b01083246a4b94bf180674af03b2e1e330283
champ <xuancq@shushangyun.com>
2020-05-25 10:23:41 +0800

    添加给某一条边添加阴影的分类

--- SwiftDemoProject/SwiftDemoProject/Extension(通用拓展)/UIKit/UIView+Extension.swift
+++ SwiftDemoProject/SwiftDemoProject/Extension(通用拓展)/UIKit/UIView+Extension.swift
@@ -132,7 +132,51 @@ extension UIView {
 }
 
 
+// MARK: -给view的某一条边添加阴影
+extension UIView {
+    enum ShadowType: Int {
+        case all = 0 ///四周
+        case top  = 1 ///上方
+        case left = 2///左边
+        case right = 3///右边
+        case bottom = 4///下方
+    }
+    ///默认设置：黑色阴影, 阴影所占视图的比例
+   // func shadow(_ type: ShadowType, percent: Float) {
+       // shadow(type: type, color: .black, opactiy: 0.4, //shadowSize: 4)
+    //}
+    ///默认设置：黑色阴影
+    func shadow(_ type: ShadowType) {
+        shadow(type: type, color: .black, opactiy: 0.4, shadowSize: 4)
+    }
+    ///常规设置
+    func shadow(type: ShadowType, color: UIColor,  opactiy: Float, shadowSize: CGFloat) -> Void {
+        layer.masksToBounds = false;//必须要等于NO否则会把阴影切割隐藏掉
+        layer.shadowColor = color.cgColor;// 阴影颜色
+        layer.shadowOpacity = opactiy;// 阴影透明度，默认0
+        layer.shadowOffset = .zero;//shadowOffset阴影偏移，默认(0, -3),这个跟shadowRadius配合使用
+        layer.shadowRadius = 3 //阴影半径，默认3
+        var shadowRect: CGRect?
+        switch type {
+        case .all:
+            shadowRect = CGRect.init(x: -shadowSize, y: -shadowSize, width: bounds.size.width + 2 * shadowSize, height: bounds.size.height + 2 * shadowSize)
+        case .top:
+            shadowRect = CGRect.init(x: -shadowSize, y: -shadowSize, width: bounds.size.width + 2 * shadowSize, height: 2 * shadowSize)
+        case .bottom:
+            shadowRect = CGRect.init(x: -shadowSize, y: bounds.size.height - shadowSize, width: bounds.size.width + 2 * shadowSize, height: 2 * shadowSize)
+        case .left:
+            shadowRect = CGRect.init(x: -shadowSize, y: -shadowSize, width: 2 * shadowSize, height: bounds.size.height + 2 * shadowSize)
+        case .right:
+            shadowRect = CGRect.init(x: bounds.size.width - shadowSize, y: -shadowSize, width: 2 * shadowSize, height: bounds.size.height + 2 * shadowSize)
+        }
+        layer.shadowPath = UIBezierPath.init(rect: shadowRect!).cgPath
+    }
+    
+}
 
+
+
+ // MARK: -给view添加点击回调
 extension UIView{
     
    typealias ViewTapActon = (_ view:UIView) -> Void
@@ -157,7 +201,6 @@ extension UIView{
 
    }
     
-   // MARK: -给view添加点击回调
    func addTapAction( action:@escaping ViewTapActon) {
     
     self.isUserInteractionEnabled = true
